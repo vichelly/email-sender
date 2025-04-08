d8ec6fca18d46d5850a49c64e41df43409b69ccf
Vitor Lucas Fujita Felício <vlucasff@hotmail.com>
2025-04-08 09:31:47 -0300

    feat: adapters

--- src/main/java/com/vitor/email_service/application/EmailSenderService.java
+++ src/main/java/com/vitor/email_service/application/EmailSenderService.java
@@ -1,21 +1,21 @@
 package com.vitor.email_service.application;
 
-import org.springframework.beans.factory.annotation.Autowired;
 
+import com.vitor.email_service.adapters.EmailSenderGateway;
 import com.vitor.email_service.core.EmailSenderUseCase;
 
+//contrato da aplicação com o nosso gateaway
 public class EmailSenderService implements EmailSenderUseCase {
 
     private final EmailSenderGateway emailSenderGateway;
 
-    @Autowired
     public EmailSenderService(EmailSenderGateway emailSenderGateway){
         this.emailSenderGateway = emailSenderGateway;
     }
 
     @Override
     public void sendEmail(String to, String subject, String body){
-
+        this.emailSenderGateway.sendEmail(to,subject,body);
     }
     
 }
